c96510cfa98e3ce2219e44c6a08ae6d463885f01
Jack Hind <jackhind@Jacks-MacBook-Pro.local>
2023-02-08 11:21:03 +0000

    Post New Comment Complete

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { postComment } from "../utils";
+import "../CSS/AddComment.css"
+
+const AddComment = ({article_id, setComments}) => {
+
+    const [newComment, setNewComment] = useState("");
+    const [commentPosted, setCommentPosted] = useState(false);
+
+    const handleNewComment = (e) => {
+        e.preventDefault();
+        postComment(article_id, newComment).then(({comment}) => {
+            setComments((currComments) => {
+                return [comment, ...currComments];
+            })
+            setCommentPosted(true)
+        })
+    }
+
+    return (
+        <section className="post-comment">
+            <form onSubmit={handleNewComment} className="post-comment-form">
+                <label htmlFor="new-comment">Add Comment:</label>
+                <textarea
+                    id="new-comment"
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}>
+                </textarea>
+                <button>POST</button>
+            </form>
+            {commentPosted ? <p>Comment Posted</p> : null}
+        </section>
+    )
+}
+
+export default AddComment;
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchComments } from "../utils";
+import AddComment from "./AddComment";
 import Comment from "./Comment";
 import "../CSS/Comments.css";
 
@@ -22,6 +23,7 @@ const Comments = ({article_id}) => {
     return (
         <section>
             <h3 className="comments-title">Comments:</h3>
+            <AddComment article_id={article_id} setComments={setComments}/>
             <ul>
                 {comments.map((comment) => {
                     return <Comment key={comment.comment_id} comment={comment}/>
